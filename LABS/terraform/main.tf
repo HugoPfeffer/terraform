05facfc844437dbebb1bebaa85f9f61344d31602
Hugo Pfeffer <hugo.pguima@gmail.com>
2020-09-30 17:00:26 -0300

    added changes to main.tf and added README, LICENSE and logo

--- LABS/terraform/main.tf
+++ LABS/terraform/main.tf
@@ -2,60 +2,102 @@ provider "aws" {
   version = "~> 3.0"
   region  = "us-east-1"
 }
+provider "aws" {
+  alias = "us-east-2"
+  version = "~> 3.0"
+  region  = "us-east-2"
+}
 
 # Create a VPC
 resource "aws_instance" "dev" {
     count = 3
-  ami = "ami-0dba2cb6798deb6d8"
+  ami = var.amis["us-east-1"]
   instance_type = "t2.micro"
-  key_name = "terraform-aws"
+  key_name = var.key_name
   tags = {
     Name = "dev${count.index}"
   }
   vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
   # vpc_security_group_ids = ["sg-9e3afbc5"]
 }
-resource "aws_instance" "dev4" {
-  ami = "ami-0dba2cb6798deb6d8"
-  instance_type = "t2.micro"
-  key_name = "terraform-aws"
-  tags = {
-    Name = "dev4"
-  }
-  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
-  depends_on = [aws_s3_bucket.dev4]
-}
+# resource "aws_instance" "dev4" {
+#   ami = var.amis["us-east-1"]
+#   instance_type = "t2.micro"
+#   key_name = var.key_name
+#   tags = {
+#     Name = "dev4"
+#   }
+#   vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+#   depends_on = [aws_s3_bucket.dev4]
+# }
 resource "aws_instance" "dev5" {
-  ami = "ami-0dba2cb6798deb6d8"
+  ami = var.amis["us-east-1"]
   instance_type = "t2.micro"
-  key_name = "terraform-aws"
+  key_name = var.key_name
   tags = {
     Name = "dev5"
   }
   vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
 }
+resource "aws_instance" "dev6" {
+  provider = aws.us-east-2
+  ami = var.amis["us-east-2"]
+  instance_type = "t2.micro"
+  key_name = var.key_name
+  tags = {
+    Name = "dev6"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}"]
+  depends_on = [aws_dynamodb_table.dynamodb-homologacao]
+}
+resource "aws_instance" "dev7" {
+  provider = aws.us-east-2
+  ami = var.amis["us-east-2"]
+  instance_type = "t2.micro"
+  key_name = var.key_name
+  tags = {
+    Name = "dev7"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-2.id}"]
+}
 
-resource "aws_s3_bucket" "dev4" {
-  bucket = "hpfefferlabs-dev4"
+
+
+# resource "aws_s3_bucket" "dev4" {
+#   bucket = "hpfefferlabs-dev4"
+#   acl    = "private"
+
+#   tags = {
+#     Name        = "hpfefferlabs-dev4"
+#   }
+# }
+
+
+resource "aws_s3_bucket" "homologacao" {
+  bucket = "hpfefferlabs-homologacao"
   acl    = "private"
 
   tags = {
-    Name        = "hpfefferlabs-dev4"
+    Name        = "hpfefferlabs-homologacao"
   }
 }
 
-resource "aws_security_group" "acesso-ssh" {
-  name        = "acesso-ssh"
-  description = "Acesso ssh"
+resource "aws_dynamodb_table" "dynamodb-homologacao" {
+  provider       = aws.us-east-2
+  name           = "GameScores"
+  billing_mode   = "PAY_PER_REQUEST"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["192.168.0.19/32" , "189.6.17.200/32"]
+  attribute {
+    name = "UserId"
+    type = "S"
   }
 
-  tags = {
-    Name = "ssh"
+  attribute {
+    name = "GameTitle"
+    type = "S"
   }
 }
---
+++ README.md
@@ -0,0 +1,48 @@
+<p align="center">
+    <img alt="Logo Codenation" src="logo.svg">
+</p>
+
+<h1 align="center">
+    Projeto de Terraform
+</h1>
+
+<h2 align="center">
+    Materiais de Estudo (Compilado por Hugo Pfeffer)
+</h2>
+
+<p align="center">
+    <img alt="CentOS Version" src="https://img.shields.io/badge/Linux-CentOS8-green">
+    <img alt="Made By" src="https://img.shields.io/badge/Made%20By-Hugo%20Pfeffer-red">
+    <img alt="License" src="https://img.shields.io/github/license/HugoPfeffer/terraform">
+    <!-- <img alt="Stargazers" src="https://img.shields.io/github/stars/HugoPfeffer/terraform?style=social"> -->
+</p>
+
+</br>
+</br>
+<h2> Projeto </h2>
+
+Projeto realizado com a finalidade de fixar meus conhecimentos na ferramenta Terraform. Além de servir como um 'playground' para futuros projetos na plataforma AWS.
+
+
+</br>
+</br>
+<hr>
+<i>Segue abaixo o conteúdo aprendido com este projeto:</i>
+
+
+
+<h4> <a href="https://www.terraform.io">Terraform</a></h4>
+<ul>
+    <li>Provisionar instancias em diferentes regiões da AWS</li>
+    <li>Utilizar variáveis</li>
+    <li>Gerar grupos de segurança</li>
+    <li>Configurar volumes</li>
+    <li>Configurar e utilizar o Terraform Cloud</li>
+</ul>
+
+
+
+
+
+
+
